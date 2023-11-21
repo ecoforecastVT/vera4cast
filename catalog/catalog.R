129ba14a2f0b95bc9515d5f8c6b4ecc8ff66a47a
addelany <addelany@gmail.com>
2023-11-21 13:01:10 -0600

    add forecast summaries collection

--- catalog/catalog.R
+++ catalog/catalog.R
@@ -45,6 +45,12 @@ build_catalog <- function(){
         "href" = 'noaa_forecasts/collection.json'
       ),
       list(
+        "rel"= "child",
+        "type"= "application/json",
+        "title"= "Summarized Forecasts",
+        "href" = 'summaries/collection.json'
+      ),
+      list(
         "rel" = "parent",
         "type" = "application/json",
         "href" = "https://raw.githubusercontent.com/eco4cast/challenge-catalogs/main/catalog.json"
