3858992d02ed9a7a915040dbff5604ebd7c98ba2
addelany <addelany@gmail.com>
2024-05-14 11:59:32 -0500

    fix summary objects

--- catalog/update_stac.R
+++ catalog/update_stac.R
@@ -7,37 +7,45 @@ library(lubridate)
 #  reticulate::use_miniconda()
 
 #Generate EFI model metadata
+print('METADATA')
 source('catalog/model_metadata.R')
 
 # catalog
+print("CATALOG TOP")
 source('catalog/catalog.R')
 
 # forecasts
+print('FORECASTS')
 source('catalog/forecasts/forecast_models.R')
 
 rm(list = ls()) # remove all environmental vars between forecast and scores
 
 # scores
+print('SCORES')
 source('catalog/scores/scores_models.R')
 
 rm(list = ls())
 
 # inventory
+print('INVENTORY')
 source('catalog/inventory/create_inventory_page.R')
 
 rm(list = ls())
 
 # summaries
+print('SUMMARIES')
 source('catalog/summaries/summaries_models.R')
 
 rm(list = ls())
 
 # targets
+print('TARGETS')
 source('catalog/targets/create_targets_page.R')
 
 rm(list = ls())
 
 # noaa
+print('NOAA')
 source('catalog/noaa_forecasts/noaa_forecasts.R')
 
 
