01773924ac26661fde9be6667f5331360ed83b3d
addelany <addelany@gmail.com>
2024-05-15 15:50:18 -0500

    fix ghg targets

--- targets/target_functions/target_generation_ghg_daily.R
+++ targets/target_functions/target_generation_ghg_daily.R
@@ -3,25 +3,25 @@
 # 24 Aug 2023
 
 target_generation_ghg_targets <- function(current_data_file, edi_data_file){
-  
-  ## read in current data file 
-  # ABP work on creating a perliminary GHG file for the current data 
+
+  ## read in current data file
+  # ABP work on creating a perliminary GHG file for the current data
   dt1 <-current_data_file
-  
-  # read in historical data file 
+
+  # read in historical data file
   # EDI
   inUrl1 <- edi_data_file
   infile1 <- tempfile()
   try(download.file(inUrl1,infile1,method="curl"))
   if (is.na(file.size(infile1))) download.file(inUrl1,infile1,method="auto")
-  
+
   # read in the data file downloaded from EDI
-  dt2 <-read_csv(infile1) 
-  
+  dt2 <-read_csv(infile1)
+
   # Filter to what you need
   targets_df<-bind_rows(dt1,dt2)%>% # bind observations together
     filter(Reservoir=="FCR"|Reservoir=="BVR")%>% # select the reservoir you want. These are hard coded in right now
-    filter(Site==50)%>% # select the site. Also hard coded in 
+    filter(Site==50)%>% # select the site. Also hard coded in
     select(-Site,-starts_with("Flag"))%>% # get rid of the columns we don't want
     group_by(Reservoir,Depth_m,DateTime)%>%
     summarise_if(is.numeric, mean, na.rm = TRUE)%>% # average if there are reps taken at a depths
@@ -39,10 +39,11 @@ target_generation_ghg_targets <- function(current_data_file, edi_data_file){
     pivot_longer(cols=c(CH4_umolL:CO2_umolL), # make the wide data frame into a long one so each observation has a depth
                  names_to='variable',
                  values_to='observation')%>%
+    mutate(variable = paste0(variable,'_sample')) |>
     select(c('datetime', 'site_id', 'depth', "observation", 'variable')) # rearrange order of columns
-  
-  
-  
+
+
+
   ## return dataframe formatted to match FLARE targets
   return(targets_df)
 }
