24b6f0646f9cfde586e61d44f7b7b3e0dba51fdc
addelany <addelany@gmail.com>
2024-07-01 13:39:42 -0500

    add additional variable arguments

--- catalog/scores/scores_models.R
+++ catalog/scores/scores_models.R
@@ -210,6 +210,7 @@ for (i in 1:length(config$variable_groups)){ # LOOP OVER VARIABLE GROUPS -- BUIL
                                        thumbnail_link = config$variable_groups[[i]]$thumbnail_link,
                                        thumbnail_title = "Thumbnail Image",
                                        group_var_vector = NULL,
+                                       single_var_name = var_name,
                                        group_duration_value = duration_value,
                                        group_sites = find_var_sites$site_id,
                                        citation_values = var_citations,
