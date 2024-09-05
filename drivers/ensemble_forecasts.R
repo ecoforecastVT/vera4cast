a368baeced8004ddba1d02adb1de095dce90c112
Quinn Thomas <rqthomas@vt.edu>
2024-09-05 10:29:41 -0400

    adding BOM ACCESS Global to the drivers download

--- drivers/ensemble_forecasts.R
+++ drivers/ensemble_forecasts.R
@@ -17,6 +17,9 @@ model_id <- "ecmwf_ifs04"
 download_ensemble_forecast(model_id, forecast_horizon = 10, sites = "fcre")
 submit_met_forecast(model_id)
 
+model_id <- "bom_access_global_ensemble"
+download_ensemble_forecast(model_id, forecast_horizon = 10, sites = "fcre")
+submit_met_forecast(model_id)
 
 ## Call healthcheck
 #RCurl::url.exists("https://hc-ping.com/d39f878d-2dc0-4373-ab01-22fa417692f8", timeout = 5)
