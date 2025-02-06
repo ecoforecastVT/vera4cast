fa36af5131ee2f8aa2d4478424b60a309853d57c
Adrienne Breef-Pilz <68744051+abreefpilz@users.noreply.github.com>
2025-02-06 11:19:20 -0500

    Updated EDI links with 2024 data

--- targets/target_generation/inflow_targets.R
+++ targets/target_generation/inflow_targets.R
@@ -17,13 +17,13 @@ source('targets/target_functions/inflow/target_generation_inflows.R')
 
 current_inflow <- 'https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-weir-data-qaqc/FCRWeir_L1.csv'
 
-historic_inflow <- "https://pasta.lternet.edu/package/data/eml/edi/202/12/aae7888d68753b276d1623680f81d5de"
+historic_inflow <- "https://pasta.lternet.edu/package/data/eml/edi/202/13/da4beae4df936f513ac7600d44871c07"
 
 historic_silica <- 'https://pasta.lternet.edu/package/data/eml/edi/542/1/791ec9ca0f1cb9361fa6a03fae8dfc95'
 
 historic_nutrients <- "https://pasta.lternet.edu/package/data/eml/edi/199/12/a33a5283120c56e90ea414e76d5b7ddb"
 
-historic_ghg <- "https://pasta.lternet.edu/package/data/eml/edi/551/8/454c11035c491710243cae0423efbe7b"
+historic_ghg <- "https://pasta.lternet.edu/package/data/eml/edi/551/9/98f19e7acae8bea7d127c463b1bb5fbc"
 
 
 inflow_daily <- target_generation_inflows(historic_inflow = historic_inflow,
