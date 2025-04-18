e97b0cd6dae2f217de01721a567c15f25027d670
addelany <addelany@gmail.com>
2025-04-18 14:09:38 -0500

    fix extra variables for baseline plots

--- baseline_models/run_persistence.R
+++ baseline_models/run_persistence.R
@@ -163,6 +163,18 @@ targets_insitu <- targets_insitu |>
                                           'SRP_ugL_sample',
                                           'NO3NO2_ugL_sample',
                                           'NH4_ugL_sample',
+                                          'DOC_mgL_sample',
+                                          'DC_mgL_sample',
+                                          'DN_mgL_mean') & site_id == 'bvre',
+                          1.5,
+                          depth_m))
+
+targets_insitu <- targets_insitu |>
+  mutate(depth_m = ifelse(variable %in% c('TN_ugL_sample',
+                                          'TP_ugL_sample',
+                                          'SRP_ugL_sample',
+                                          'NO3NO2_ugL_sample',
+                                          'NH4_ugL_sample',
                                           'DOC_mgL_sample') & site_id == 'bvre',
                           1.5,
                           depth_m))
