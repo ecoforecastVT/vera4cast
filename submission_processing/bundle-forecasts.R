8092ea4ed5818887b38fc790d61ba0bec92bc2bc
addelany <addelany@gmail.com>
2025-07-22 10:48:05 -0500

    update forecast bundles with new code

--- submission_processing/bundle-forecasts.R
+++ submission_processing/bundle-forecasts.R
@@ -16,15 +16,11 @@ mc_alias_set("osn", "amnh1.osn.mghpcc.org", Sys.getenv("OSN_KEY"), Sys.getenv("O
 
 duckdb_secrets(endpoint = "amnh1.osn.mghpcc.org", key = Sys.getenv("OSN_KEY"), secret = Sys.getenv("OSN_SECRET"), bucket = "bio230121-bucket01")
 
-
+# bundled count at start
 open_dataset("s3://bio230121-bucket01/vera4cast/forecasts/bundled-parquet",
              s3_endpoint = "amnh1.osn.mghpcc.org",
              anonymous = TRUE) |>
   count()
-
-# make sure new-forecasts location exists and is empty.
-fs::dir_create("new-forecasts"); fs::dir_delete("new-forecasts")
-fs::dir_create("new-forecasts/bundled-parquet")
 
 remote_path <- "osn/bio230121-bucket01/vera4cast/forecasts/parquet/project_id=vera4cast/"
 contents <- mc_ls(remote_path, recursive = TRUE, details = TRUE)
@@ -117,58 +113,54 @@ bundle_me <- function(path) {
   print(path)
   con = duckdbfs::cached_connection(tempfile())
   duckdb_secrets(endpoint = "amnh1.osn.mghpcc.org", key = Sys.getenv("OSN_KEY"), secret = Sys.getenv("OSN_SECRET"), bucket = "bio230121-bucket01")
+  bundled_path <- path |> str_replace(fixed("forecasts/parquet"), "forecasts/bundled-parquet")
 
-  new <- open_dataset(path, conn = con, unify_schemas = TRUE) |>
-            filter( !is.na(model_id),
-                    !is.na(parameter),
-                    !is.na(prediction)) |>
-            select(-any_of(c("date", "reference_date", "...1")))  # (date is a short version of datetime from partitioning, drop it)
-
-  new |> write_dataset("tmp_new.parquet")
-
-  bundled_path <- path |>
-    str_replace(fixed("forecasts/parquet"), "forecasts/bundled-parquet")
-
-
-  old <-
-    open_dataset(bundled_path, conn = con) |>
+  open_dataset(path, conn = con, unify_schemas = TRUE) |>
     filter( !is.na(model_id),
             !is.na(parameter),
             !is.na(prediction)) |>
     select(-any_of(c("date", "reference_date", "...1"))) |>
-    write_dataset("tmp_old.parquet")
+    write_dataset("tmp_new.parquet")
 
-  by <- join_by(datetime, site_id, prediction, parameter, family,
-                reference_datetime, pub_datetime, duration, model_id,
-                project_id, variable)
+  # special filters should not be needed on bundled copy
+  open_dataset(bundled_path, conn = con) |>
+    write_dataset("tmp_old.parquet")
 
   # these are both local, so we can stream back.
   new <- open_dataset("tmp_new.parquet")
-  old <- open_dataset("tmp_old.parquet") |>
-    anti_join(new, by = by)
+  old <- open_dataset("tmp_old.parquet")
+
+  ## We can just "append", we no longer face duplicates:
+  # by <- join_by(datetime, site_id, prediction, parameter, family, reference_datetime, pub_datetime, duration, model_id, project_id, variable)
+  #  filtered_n <- old |> anti_join(new, by = by) |> count() |> pull(n) # is this the bottleneck?
+  #  previous_n <- open_dataset("tmp_old.parquet") |> count() |> pull(n)
+  #  stopifnot(previous_n - filtered_n == 0)
 
   union_all(old, new) |>
     write_dataset(bundled_path,
                   options = list("PER_THREAD_OUTPUT false"))
 
   #We should now archive anything we have bundled:
-
   mc_path <- path |> str_replace(fixed("s3://"), "osn/")
   dest_path <- mc_path |>
     str_replace(fixed("forecasts/parquet"), "forecasts/archive-parquet")
   mc_mv(mc_path, dest_path, recursive = TRUE)
 
   # clears up empty folders (not necessary?)
-
   #mc_rm(mc_path, recursive = TRUE)
   remove_dir(mc_path)
 
   duckdbfs::close_connection(con); gc()
+
+  invisible(0)
 }
 
+try_bundles <- purrr::possibly(bundle_me)
+
 bench::bench_time({
-  lapply(model_paths, bundle_me)
+  out <- purrr::map(model_paths, try_bundles)
 })
+
 
 #
 # #############################################################################
