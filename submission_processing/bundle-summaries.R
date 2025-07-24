5c434ac6fbcd1b8c6eb0f9f668d8fdfdeb919276
addelany <addelany@gmail.com>
2025-07-24 15:35:30 -0500

    add more flexibility for new summary bundles

--- submission_processing/bundle-summaries.R
+++ submission_processing/bundle-summaries.R
@@ -44,6 +44,38 @@ model_paths <-
   str_replace("^osn\\/", "s3://") |>
   unique()
 
+remove_dir <- function(path) {
+  tryCatch(
+    {
+      minioclient::mc_rm(path, recursive = TRUE)
+      message('directory successfully removed...')
+    },
+    error = function(cond) {
+      message("The removal directory could not be found...")
+      message("Here's the original error message:")
+      message(conditionMessage(cond))
+      # Choose a return value in case of error
+      NA
+    },
+    warning = function(cond) {
+      message('Deleting the directory caused a warning...')
+      message("Here's the original warning message:")
+      message(conditionMessage(cond))
+      # Choose a return value in case of warning
+      NULL
+    },
+    finally = {
+      # NOTE:
+      # Here goes everything that should be executed at the end,
+      # regardless of success or error.
+      # If you want more than one expression to be executed, then you
+      # need to wrap them in curly brackets ({...}); otherwise you could
+      # just have written 'finally = <expression>'
+      message("Finished the delete portion...")
+    }
+  )
+}
+
 
 bundle_me <- function(path) {
 
@@ -56,15 +88,26 @@ bundle_me <- function(path) {
     str_replace(fixed("forecasts/summaries"), "forecasts/bundled-summaries")
 
   open_dataset(path, conn = con) |> write_dataset("tmp_new.parquet")
-  open_dataset(bundled_path, conn = con) |> write_dataset("tmp_old.parquet")
 
-  # these are both local, so we can stream back.
-  new <- open_dataset("tmp_new.parquet")
-  old <- open_dataset("tmp_old.parquet")
+  files_exist <- minioclient::mc_ls(bundled_path |> str_replace(fixed("s3://"), "osn/"))
 
-  union_all(old, new) |>
-    write_dataset(bundled_path,
-                  options = list("PER_THREAD_OUTPUT false"))
+  if (length(files_exist > 0)){
+    open_dataset(bundled_path, conn = con) |> write_dataset("tmp_old.parquet")
+
+    # these are both local, so we can stream back.
+    new <- open_dataset("tmp_new.parquet")
+    old <- open_dataset("tmp_old.parquet")
+
+    union_all(old, new) |>
+      write_dataset(bundled_path,
+                    options = list("PER_THREAD_OUTPUT false"))
+  } else {
+    new <- open_dataset("tmp_new.parquet")
+
+    new |>
+      write_dataset(bundled_path,
+                    options = list("PER_THREAD_OUTPUT false"))
+  }
 
   #We should now archive anything we have bundled:
 
@@ -73,7 +116,9 @@ bundle_me <- function(path) {
     str_replace(fixed("forecasts/summaries"), "forecasts/archive-parquet-summaries")
   mc_mv(mc_path, dest_path, recursive = TRUE)
   # clears up empty folders
-  mc_rm(mc_path, recursive = TRUE)
+  #mc_rm(mc_path, recursive = TRUE)
+  remove_dir(mc_path)
+
 
   duckdbfs::close_connection(con); gc()
   invisible(path)
